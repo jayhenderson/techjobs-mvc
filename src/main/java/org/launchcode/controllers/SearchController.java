52be175155e231ece56bc76361956fd2aadf286d
Jay Henderson <jjhenderson@gmail.com>
2017-03-15 23:06:18 -0500

    Work on results handler

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -23,5 +24,9 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value="/results", method = RequestMethod.GET)
+    public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+        return "redirect:";
+    }
 }
